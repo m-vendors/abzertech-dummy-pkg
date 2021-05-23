bbdc8cefe4745e34d1f7f6bd82d7c31eff40236a
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:09:23 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'abzertech_DummyPkg',
+    __DIR__
+);
